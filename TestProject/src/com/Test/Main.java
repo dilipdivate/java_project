6447196fa42dcf9f3cc6cb4d426725de92247316
dilipdivate <dilip.divate@gmail.com>
2023-04-21 22:58:27 +1000

    First Git commit

--- TestProject/src/com/Test/Main.java
+++ TestProject/src/com/Test/Main.java
@@ -1,10 +1,25 @@
 package com.Test;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is test project");
+		
+		ArrayList<Integer> items = new ArrayList<>();
+		items.add(1);
+		items.add(2);
+		items.add(3);
+		items.add(4);
+		
+		printDoubled(items);
+	}
+	
+	public static void printDoubled(ArrayList<Integer> n) {
+		for (int i : n) {
+			System.out.println(i * 2);
+		}
+		System.out.println( n);
 	}
 
 }
