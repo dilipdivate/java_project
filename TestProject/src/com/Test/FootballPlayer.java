d732be50f0fdc78eb3a19ef0685e50fb764ae2ad
dilipdivate <dilip.divate@gmail.com>
2023-04-22 00:07:57 +1000

    Generics added

---
+++ TestProject/src/com/Test/FootballPlayer.java
@@ -0,0 +1,7 @@
+package com.Test;
+
+public class FootballPlayer extends Player{
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
--- TestProject/src/com/Test/Main.java
+++ TestProject/src/com/Test/Main.java
@@ -1,25 +1,60 @@
 package com.Test;
 
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> items = new ArrayList<>();
-		items.add(1);
-		items.add(2);
-		items.add(3);
-		items.add(4);
-		
-		printDoubled(items);
-	}
-	
-	public static void printDoubled(ArrayList<Integer> n) {
-		for (int i : n) {
-			System.out.println(i * 2);
-		}
-		System.out.println( n);
+
+		FootballPlayer joe = new FootballPlayer("Joe");
+		FootballPlayer pat = new FootballPlayer("Pat");
+
+		FootballPlayer messi = new FootballPlayer("Messi");
+		FootballPlayer rati = new FootballPlayer("Rati");
+
+		CricketPlayer dhoni = new CricketPlayer("Dhoni");
+		CricketPlayer yuvi = new CricketPlayer("Yuvi");
+
+		CricketPlayer ponting = new CricketPlayer("Ponting");
+		CricketPlayer mcgrath = new CricketPlayer("Mcgrath");
+
+		Team<CricketPlayer> india = new Team<>("India");
+		Team<CricketPlayer> australia = new Team<>("Australia");
+
+		Team<FootballPlayer> brazil = new Team<>("Brazil");
+		Team<FootballPlayer> argentina = new Team<>("Argentina");
+
+		india.addPlayer(dhoni);
+		india.addPlayer(yuvi);
+		australia.addPlayer(ponting);
+		australia.addPlayer(mcgrath);
+
+		australia.addPlayer(dhoni);
+		brazil.addPlayer(joe);
+		brazil.addPlayer(pat);
+		argentina.addPlayer(messi);
+		argentina.addPlayer(rati);
+
+		india.matchResult(australia, 1, 0);
+		brazil.matchResult(argentina, 3, 8);
+
+		System.out.println(india.numPlayers());
+		System.out.println(australia.numPlayers());
+		System.out.println(brazil.numPlayers());
+		System.out.println(argentina.numPlayers());
+
+		System.out.println("Rankings");
+		System.out.println(india.getName() + ": " + india.ranking());
+		System.out.println(australia.getName() + ": " + australia.ranking());
+		System.out.println(brazil.getName() + ": " + brazil.ranking());
+		System.out.println(argentina.getName() + ": " + argentina.ranking());
+
+		System.out.println(india.compareTo(australia));
+		System.out.println(brazil.compareTo(argentina));
+		System.out.println(argentina.compareTo(brazil));
+		System.out.println(australia.compareTo(india));
+
+//		ArrayList<Team> teams;
+//		Collections.sort(teams);
+
 	}
 
 }
